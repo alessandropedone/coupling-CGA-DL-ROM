1c2d5f9db519e748fd6b586288aace72a06505ec
alessandropedone <alessandro.pedone02@gmail.com>
2025-05-22 23:21:46 +0200

    restore main.py

---
+++ src/surrogate_model/main.py
@@ -0,0 +1,101 @@
+from plot_prediction import plot_random_prediction
+import tensorflow as tf
+import datetime
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from model import NN_Model
+
+def train_model():
+    # Import coordinates dataset and convert to numpy array
+    coordinates = pd.read_csv('data/coordinates.csv')
+    x = coordinates.iloc[:, 1:]
+    x = x.to_numpy()
+
+    # Import normal derivative potential dataset and convert to numpy array
+    normal_derivative = pd.read_csv('data/normal_derivative_potential.csv')
+    y = normal_derivative.iloc[:, 4:]
+    y = y.to_numpy()
+
+    # Split into train+val and test sets first
+    x_trainval, x_test, y_trainval, y_test = train_test_split(
+        x, y, test_size=0.15, random_state=42
+    )
+
+    # Split train+val into train and val sets
+    x_train, x_val, y_train, y_val = train_test_split(
+        x_trainval, y_trainval, test_size=0.15, random_state=42
+    )
+
+    print("x_train shape:", x_train.shape)
+    print("y_train shape:", y_train.shape)
+    print("x_val shape:", x_val.shape)
+    print("y_val shape:", y_val.shape)
+    print("x_test shape:", x_test.shape)
+    print("y_test shape:", y_test.shape)
+
+    # Mixed Precision Setup
+    policy = tf.keras.mixed_precision.Policy('mixed_float16')
+    tf.keras.mixed_precision.set_global_policy(policy)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Specify which GPU to use
+    gpus = tf.config.list_physical_devices('GPU')
+    with tf.device('/GPU:0' if gpus else '/CPU:0'):
+        # Check if the GPU is available
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus:
+            print("Using GPU for training and evaluation")
+        else:
+            print("No GPU detected, using CPU")
+
+        model = NN_Model()
+
+        model.build_model(
+            X = x_train,
+            input_shape = 353, 
+            n_neurons = [512, 256, 128], 
+            activation = 'relu',
+            output_neurons = 350,
+            output_activation = 'linear',
+            initializer = 'he_normal',
+            lambda_coeff = 1e-4,
+            batch_normalization = True,
+            dropout = True,
+            dropout_rate = 0.2
+        )
+
+        model.summary()
+
+        lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=1)
+
+        model.train_model(
+            X = x_train, 
+            y = y_train, 
+            X_val = x_val, 
+            y_val = y_val, 
+            learning_rate = 1e-4, 
+            epochs = 1000, 
+            batch_size = 32, 
+            loss = 'mean_squared_error', 
+            validation_freq = 1, 
+            verbose = 1,
+            lr_scheduler = lr_scheduler,
+            metrics = ['mae'],
+            clipnorm = 1.0,
+            early_stopping_patience = 20,
+            log = True
+        )
+
+        print("Evaluating the model on the validation set...")
+        model.evaluate_model(X = x_val, y = y_val)
+
+        print("Evaluating the model on the test set...")
+        model.evaluate_model(X = x_test, y = y_test)
+
+        model.save_model("surrogate_model.keras")
+    
+
+def test_model():
+    plot_random_prediction("surrogate_model.keras")
+
+test_model()
