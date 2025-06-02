eacfa0c9dad9593e7512da41ac30e0e984a36600
alessandropedone <alessandro.pedone02@gmail.com>
2025-06-02 19:40:03 +0200

    remove test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("Built with CUDA:", tf.test.is_built_with_cuda())
-print("GPU devices found:", tf.config.list_physical_devices('GPU'))
-
